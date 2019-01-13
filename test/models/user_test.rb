593cbc0159af67d3b50030a4db5216a05cb2a875
kumar <kumarrockstarsaurabh@gmail.com>
2019-01-14 01:57:08 +0530

    Added HAS_SECURE_PASSWORD function and min password length vaidation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: "Example User", email: "sandbox@kumar.ninja")
+    @user = User.new(name: "Example User", email: "sandbox@kumar.ninja", password: "sbxsbx", password_confirmation: "sbxsbx")
   end
 
   test "should be valid" do
@@ -58,5 +58,20 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "k" * 5
+    assert_not @user.valid?
+  end
+
+  test "super simple password be rejected" do
+    @user.password = @user.password_confirmation = "qwerty"
+    assert_not @user.valid?
   end
 end
