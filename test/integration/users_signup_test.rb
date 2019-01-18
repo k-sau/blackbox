d0295ebb0119e98202a6dc1cb9801099af21463f
kumar <kumarrockstarsaurabh@gmail.com>
2019-01-19 00:37:59 +0530

    Added a test case for signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "invalid signup should be rejected" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: {
+        user: {
+          name: "",
+          email: "user@kumar",
+          password: "test",
+          password_confirmation: "bar"
+        }
+      }
+    end
+    assert_template 'users/new'
+    assert_select 'div.alert-danger'
+  end
 end
