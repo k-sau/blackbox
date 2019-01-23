dc96ae4cf58f8e815785bf7b44d6f9f253da12e4
kumar <kumarrockstarsaurabh@gmail.com>
2019-01-23 22:26:27 +0530

    Implemented basic login with temporary cookiw

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.0'
 
 gem 'rails', '~> 5.2.2'
 gem 'bcrypt', '3.1.12'
+gem 'jquery-rails', '~> 4.3'
 gem 'bootstrap-sass', '3.3.7'
 gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    def hello_world
-        render html: "hello, world!"
-    end
+    protect_from_forgery with: :exception
+    include SessionsHelper
     
 end
